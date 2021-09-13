086a414d165f06d76fa08caab522980c1130ce3a
dpbam <derekphtpt@gmail.com>
2021-09-12 19:53:38 -0600

    manifest.json and fixed service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
-const APP_PREFIX = 'BudgetTracker2000-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION
+const APP_PREFIX = "BudgetTracker2000-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "/",
+  "/",
   "./index.html",
   "./manifest.json",
   "./css/style.css",
-//   "./assets/css/bootstrap.css",
+  //   "./assets/css/bootstrap.css",
   "./js/idb.js",
   "./js/index.js",
   "./icons/icon-72x72.png",
@@ -19,73 +19,95 @@ const FILES_TO_CACHE = [
 ];
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-    if (e.request.url.includes("/api")) {
-        e.respondWith(
-            caches
-                .open(DATA_CACHE_NAME)
-                .then((cache) => {
-                    return fetch(e.request)
-                        .then((response) => {
-                            if (response.status === 200) {
-                                cache.put(e.request.url, response.clone());
-                            }
-                            return response;
-                        })
-                        .catch((err) => {
-                            return cache.match(e.request);
-                        });
-                })
-                .catch((err) => console.log(err))
-        );
-        return;
-    }
-    e.respondWith(
-        fetch(e.request).catch(function () {
-            return caches.match(e.request).then(function (response) {
-                if (response) { // if cache is available, respond with cache
-                    console.log('responding with cache : ' + e.request.url)
-                    return response;
-                } else {       // if there are no cache, try fetching request
-                    console.log('file is not cached, fetching : ' + e.request.url)
-                    return fetch(e.request)
-                }
-            })
-        })
-    );
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
 });
 
+// Respond with cached resources
+// self.addEventListener("fetch", function (e) {
+//   console.log("fetch request : " + e.request.url);
+//   if (e.request.url.includes("/api")) {
+//     e.respondWith(
+//       caches
+//         .open(DATA_CACHE_NAME)
+//         .then((cache) => {
+//           return fetch(e.request)
+//             .then((response) => {
+//               if (response.status === 200) {
+//                 cache.put(e.request.url, response.clone());
+//               }
+//               return response;
+//             })
+//             .catch((err) => {
+//               return cache.match(e.request);
+//             });
+//         })
+//         .catch((err) => console.log(err))
+//     );
+//     return;
+//   }
+//   e.respondWith(
+//     fetch(e.request).catch(function () {
+//       return caches.match(e.request).then(function (response) {
+//         if (response) {
+//           // if cache is available, respond with cache
+//           console.log("responding with cache : " + e.request.url);
+//           return response;
+//         } else {
+//           // if there are no cache, try fetching request
+//           console.log("file is not cached, fetching : " + e.request.url);
+//           return fetch(e.request);
+//         }
+//       });
+//     })
+//   );
+// });
+
 // Cache resources
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 
   self.skipWaiting();
-})
+});
 
 // Delete outdated caches
-self.addEventListener('activate', function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       // `keyList` contains all cache names under your username.github.io
       // filter out ones that has this app prefix to create keeplist
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
-      })
+      });
       // add current cache name to keeplist
       cacheKeeplist.push(CACHE_NAME);
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i]);
-        }
-      }));
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
     })
   );
 });
