eae647e39b84098d685481c647ee1ed60acb771f
dpbam <derekphtpt@gmail.com>
2021-09-12 20:17:47 -0600

    worked on idb.js trying to get the correct route

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,66 +1,64 @@
 let db;
 
-const request = indexedDB.open('budget-tracker-2000', 1);
+const request = indexedDB.open("budget-tracker-2000", 1);
 
-request.onupgradeneeded = function(event) {
-    const db = event.target.result;
-    db.createObjectStore('deposit_withdraw', { autoIncrement: true });
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("deposit_withdraw", { autoIncrement: true });
 };
 
 // upon a successful thing
-request.onsuccess = function(event) {
-    db = event.target.result;
+request.onsuccess = function (event) {
+  db = event.target.result;
 
-    if (navigator.onLine) {
-        uploadMoney();
-    }
+  if (navigator.onLine) {
+    uploadMoney();
+  }
 };
 
-request.onerror = function(event) {
-    console.log(event.target.errorCode);
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['deposit_withdraw'], 'readwrite');
+  const transaction = db.transaction(["deposit_withdraw"], "readwrite");
 
-    const moneyObjectStore = transaction.objectStore('deposit_withdraw');
+  const moneyObjectStore = transaction.objectStore("deposit_withdraw");
 
-    moneyObjectStore.add(record);
+  moneyObjectStore.add(record);
 }
 
 function uploadMoney() {
-    const transaction = db.transaction(['deposit_withdraw'], 'readwrite');
-    const moneyObjectStore = transaction.objectStore('deposit_withdraw');
-    const getAll = moneyObjectStore.getAll();
+  const transaction = db.transaction(["deposit_withdraw"], "readwrite");
+  const moneyObjectStore = transaction.objectStore("deposit_withdraw");
+  const getAll = moneyObjectStore.getAll();
 
-    getAll.onsuccess = function() {
-        if (getAll.result.length > 0) {
-            fetch('/api/pizzas', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept:
-                        'application/json, text/plain, */*',
-                        'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if (serverResponse.message) {
-                        throw new Error(serverResponse);
-                    }
-                    const transaction = db.transaction(['deposit_withdraw'], 'readwrite')
-                    const moneyObjectStore = transaction.objectStore('deposit_withdraw');
-                    moneyObjectStore.clear();
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["deposit_withdraw"], "readwrite");
+          const moneyObjectStore = transaction.objectStore("deposit_withdraw");
+          moneyObjectStore.clear();
 
-                    alert('All saved money has been submitted.');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
+          alert("All saved money has been submitted.");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
+  };
 }
 
-window.addEventListener('online', uploadMoney);
-
+window.addEventListener("online", uploadMoney);
